a0ed13fec42671e69990b7f3c9a5a9d5f92d70a5
BryanEloy <bryanzataray@gmail.com>
2026-06-29 18:31:45 -0600

    Fix DB errors

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
